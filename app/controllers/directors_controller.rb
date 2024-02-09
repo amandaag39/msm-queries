680e033b37df53976e0991f3dcfaab9b24bb20d2
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-09 22:36:57 +0000

    Adds directors#youngest #eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -12,4 +12,30 @@ class DirectorsController < ApplicationController
     
     render({ :template => "director_templates/details"})
   end
+
+  def junior
+    # @latest_dob = Director.maximum(:dob)
+
+    # @the_youngest_name = Director.find_by(dob: @latest_dob).name
+
+    # @the_youngest_id = Director.find_by(dob: @latest_dob).id
+    
+    youngest_director = Director.find_by(dob: Director.maximum(:dob))
+
+    @the_youngest_name = youngest_director.name
+    @the_youngest_id = youngest_director.id
+    @latest_dob = youngest_director.dob
+
+    render({ :template => "director_templates/youngest"})
+  end
+
+  def senior
+    eldest_director = Director.find_by(dob: Director.minimum(:dob))
+
+    @the_eldest_name = eldest_director.name
+    @the_eldest_id = eldest_director.id
+    @earliest_dob = eldest_director.dob
+
+    render({ :template => "director_templates/eldest"})
+  end
 end
